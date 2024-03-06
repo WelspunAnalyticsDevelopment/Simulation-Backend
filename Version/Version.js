ab70f635ca21f699b97f0dae860930a739ab6d97
Tushar Pant <tushar_pant@welspun.com>
2024-03-06 16:14:02 +0530

    final folder structure

--- Version/Version.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json(["version 1","version 2","version 3"]);
-});
-
-module.exports = router;
